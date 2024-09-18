e2f23cbb2b13bd842074a3a295df0b2df96d3a14
Марина Васильева <marina_paley@mail.ru>
2024-09-18 15:56:16 +0300

    Add project files.

---
+++ Array.Tests/Array.Tests.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ArrayTests
+{
+	TEST_CLASS(ArrayTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
